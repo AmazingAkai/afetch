fb80dfefeee85eeee2faf3479f2baafae670c9f7
AmazingAkai <azharmomin420@gmail.com>
2023-07-04 21:06:02 +0530

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,94 @@
-use std::process::Command;
 
-fn get_distribution() -> String {
-    if std::path::Path::new("/etc/os-release").exists() {
-        let output = Command::new("sh")
-            .arg("-c")
-            .arg("source /etc/os-release && echo $PRETTY_NAME")
-            .output()
-            .expect("Failed to execute command");
-
-        return String::from_utf8_lossy(&output.stdout).trim().to_string();
-    }
-
-    String::from(std::env::consts::OS).trim().to_string()
-}
-
-fn get_kernel_version() -> String {
-    let output = Command::new("uname")
-        .arg("-r")
-        .output()
-        .expect("Failed to execute command");
-
-    String::from_utf8_lossy(&output.stdout).trim().to_string()
-}
-
-fn get_package_count(package_manager: &str) -> String {
-    if package_manager == "N/A" {
-        return String::from("N/A");
-    }
-
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(format!("{} -qa | wc -l", package_manager))
-        .output()
-        .expect("Failed to execute command");
-
-    let count = String::from_utf8_lossy(&output.stdout).trim().to_string();
-    format!("{} ({})", count, package_manager)
-}
-
-fn get_window_manager() -> String {
-    std::env::var("XDG_CURRENT_DESKTOP")
-        .or_else(|_| std::env::var("DESKTOP_SESSION"))
-        .unwrap_or_else(|_| String::from("N/A"))
-}
-
-fn print_system_info() {
-    let cyan: &str = "\x1b[0;96m";
-    let pink: &str = "\x1b[0;95m";
-    let green: &str = "\x1b[0;92m";
-    let red: &str = "\x1b[0;91m";
-    let lavender: &str = "\x1b[0;94m";
-    let peach: &str = "\x1b[0;93m";
-    let nc: &str = "\x1b[0m";
-    let dot: &str = "󰝥";
-
-    let distribution = get_distribution();
-    let kernel = get_kernel_version();
-
-    let package_manager: &str = match distribution.as_str() {
-        s if s.contains("Ubuntu") || s.contains("Debian") => "dpkg",
-        s if s.contains("Fedora") || s.contains("Red Hat") => "rpm",
-        _ => "N/A",
-    };
-
-
-    let symbol: &str = match distribution.as_str() {
-        s if s.contains("Debian") => "",
-        s if s.contains("Ubuntu")=> "",
-        s if s.contains("Fedora")=> "",
-        s if s.contains("Red Hat")=> "󱄛",
-        s if s.contains("Arch")=> "󰣇",
-        _ => "N/A",
-    };
-
-    let package_count = get_package_count(package_manager);
-    let window_manager = get_window_manager();
-
-    println!(
-        r#"
-         {cyan}{symbol}{nc} {distribution}
- /\_/\   {pink}{nc} {kernel}
-(=^ω^=)  {green}󰏔{nc} {package_count}
-(\_/)    {red}󱡶{nc} {window_manager}
-         {green}{dot}{nc} {pink}{dot}{nc} {red}{dot}{nc} {cyan}{dot}{nc} {lavender}{dot}{nc} {peach}{dot}{nc}
-"#);
-}
+use sysinfo::{System, SystemExt};
+use std::env;
 
 fn main() {
-    print_system_info();
+    // Penguin Colours
+    const BLACK: &str = "\x1b[30m";
+    const WHITE: &str = "\x1b[37m";
+    const YELLOW: &str = "\x1b[33m";
+
+    // Text Colours
+    const CYAN: &str = "\x1b[0;96m";
+    const PINK: &str = "\x1b[0;95m";
+    const GREEN: &str = "\x1b[0;92m";
+    const RED: &str = "\x1b[0;91m";
+    const LAVENDER: &str = "\x1b[0;94m";
+    const PEACH: &str = "\x1b[0;93m";
+
+    // Misc
+    const DOT: &str = "";
+    const NC: &str = "\x1b[0m";
+
+    println!("");
+    println!("{}   _~_   {}  {}os:{} {}", BLACK, WHITE, CYAN, NC, get_os_name());
+    println!("{}  (o o)  {}  {}shell:{} {}", BLACK, WHITE, PINK, NC, get_shell_name());
+    println!("{} /  V  \\ {}  {}wm:{} {}", YELLOW, WHITE, GREEN, NC, get_desktop_environment());
+    println!(
+        "{}/(  _  )\\{}  {}ram:{} {}",
+        YELLOW, WHITE, RED, NC, get_ram_usage()
+    );
+    println!(
+        "{}  ^^ ^^  {}   {}{}{} {}{}{} {}{}{} {}{}{} {}{}{} {}{}{}",
+        WHITE, WHITE, CYAN, DOT, NC, PINK, DOT, NC, GREEN, DOT, NC, RED, DOT, NC, LAVENDER, DOT, NC, PEACH, DOT, NC
+    );
+    println!("");
+}
+
+fn get_ram_usage() -> String {
+    let mut system = System::new_all();
+    system.refresh_memory();
+
+    let total_ram_mb = system.total_memory() / (1024 * 1024);
+    let used_ram_mb = system.used_memory() / (1024 * 1024);
+
+    format!("{} MB / {} MB", used_ram_mb, total_ram_mb)
+}
+
+fn get_os_name() -> String {
+    let mut system = System::new_all();
+    system.refresh_system();
+
+    if let Some(name) = system.name() {
+        name
+    } else {
+        "Not Found".to_string()
+    }
+}
+fn get_shell_name() -> String {
+    if let Ok(shell) = env::var("SHELL") {
+        if let Some(file_name) = std::path::Path::new(&shell).file_name() {
+            if let Some(name) = file_name.to_str() {
+                return String::from(name);
+            }
+        }
+    }
+
+    String::from("bash")
+}
+
+
+
+fn get_desktop_environment() -> String {
+    if cfg!(target_os = "linux") {
+        if let Some(de_name) = get_de_name() {
+            return de_name;
+        } else if let Some(wm_name) = get_wm_name() {
+            return wm_name;
+        }
+    } else if cfg!(target_os = "windows") {
+        return "Desktop Window Manager".to_string();
+    } else if cfg!(target_os = "macos") {
+        return "Aqua".to_string();
+    }
+
+    String::from("None")
+}
+
+fn get_de_name() -> Option<String> {
+    env::var("XDG_CURRENT_DESKTOP").ok()
+}
+
+fn get_wm_name() -> Option<String> {
+    env::var("XDG_SESSION_DESKTOP").ok()
 }
